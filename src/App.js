d0c5376a343a41848cf5a77a8979a7a714a9b85f
Stephen <skairu09@gmail.com>
2022-11-08 22:01:53 +0300

    Add navlinks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
+import Services from "./pages/Services"
+import Navbar from "./components/Navbar"
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
       
     </div>
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,9 +1,13 @@
 import React from 'react'
+// import ServiceCard from '../components/ServiceCard'
+import Navbar from './../components/Navbar'
 
 const Services = () => {
   return (
     <div>
         <Navbar />
+        {/* <ServiceCard /> */}
+        <img src="https://img.freepik.com/free-photo/beautiful-african-woman-smiling-resting-relaxing-spa-salon_176420-12883.jpg?size=626&ext=jpg&ga=GA1.2.790298598.1667807727&semt=sph" alt="Massage" />
     </div>
   )
 }
