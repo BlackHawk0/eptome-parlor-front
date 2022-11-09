8df9a70505712321d99b620c00f42143cd7212e3
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-11-09 16:40:35 +0300

    this commit adds the booking page to the project

--- cards/src/components/booking.js
+++ cards/src/components/booking.js
@@ -1,66 +1,61 @@
-// import React, {useState} from "react";
-// import Axios from "axios"
+import React, {useState} from "react";
+import Axios from "axios"
 
 
-// function NewPost() {
+function NewPost() {
 
-//     // const [addText, setAddText] = useState('')
-//     const url = 'https://lit-depths-86097.herokuapp.com/blogs'
-//     const [data, setData] = useState({
-//         name: ' ',
-//         email: ' ',
-//         Tel: ' ',
-//         date: ' ',
-//         service : ' '
-//       })
+    // const [addText, setAddText] = useState('')
+    const url = 'https://lit-depths-86097.herokuapp.com/blogs'
+    const [data, setData] = useState({
+        name: ' ',
+        email: ' ',
+        Tel: ' ',
+        date: ' ',
+        service : ' '
+      })
 
-//       function handle(e) {
-//         const newData = {...data}
-//         newData[e.target.id] = [e.target.value]
-//         setData(newData)
-//         console.log(newData)
-//       }
-    
+      function handle(e) {
+        const newData = {...data}
+        newData[e.target.id] = [e.target.value]
+        setData(newData)
+        console.log(newData)
+      }
 
-//       function addDataForm(e) {
-//         e.preventDefault()
+
+      function addDataForm(e) {
+        e.preventDefault()
+
         
-//         Axios.post(url, {
-//           name: data.customer_name,
-//           email : data.customer_email,
-//           tel : data.customer_phonenumber,
-//           service : data.likes
-//         })
-//         .then(res=>console.log(res))
+        Axios.post(url, {
+          name: data.customer,
+          email : data.customer_email,
+          tel : data.customer_phonenumber,
+          date: data.booked_date,
+          service : data.service_id
+        })
+        .then(res=>console.log(res))
 
-//         e.reset()
-//     }
+        e.reset()
+    }
     
-//     t.string "customer_name"
-//     t.string "customer_phonenumber"
-//     t.string "customer_email"
-//     t.datetime "booked_date", precision: nil
-//     t.integer "service_id"
-//     t.datetime "created_at", null: false
-//     t.datetime "updated_at", null: false
 
-//     return(
-//         <div className="div">
-//             <h3>New Posts</h3>
-//             <br />
-//             <form  onSubmit={(e)=>addDataForm(e)}>
-//                 <p className="label">Title</p><input onChange={(e)=>handle(e)} type='text' id="title" value={data.title} required name="title" placeholder="title" />
+    return(
+        <div className="div">
+            <h3>New Posts</h3>
+            <br />
+            <form  onSubmit={(e)=>addDataForm(e)}>
+                <p className="label">Title</p><input onChange={(e)=>handle(e)} type='text' id="title" value={data.title} required name="title" placeholder="title" />
                 
-//                 <p className="label">Content</p>
-//                 <input onChange={(e)=>handle(e)} type='text' id="content" value={data.content} required name="content" placeholder="enter your content"/>
+                <p className="label">Content</p>
+                <input onChange={(e)=>handle(e)} type='text' id="content" value={data.content} required name="content" placeholder="enter your content"/>
                 
-//                 <p className="label">Author</p>
+                <p className="label">Author</p>
                 
-//                 <input onChange={(e)=>handle(e)} type='text' id="author" value={data.author} required name="author" placeholder=" ~anonymous"/>
+                <input onChange={(e)=>handle(e)} type='text' id="author" value={data.author} required name="author" placeholder=" ~anonymous"/>
                 
-//                 <input onChange={(e)=>handle(e)} type='submit'  className="submit"/>
-//             </form>
-//         </div>
-//     )
-// }
-// export default NewPost
+                <input onChange={(e)=>handle(e)} type='submit'  className="submit"/>
+            </form>
+        </div>
+    )
+}
+export default NewPost
