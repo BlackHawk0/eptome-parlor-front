6e4ee522be0a568465e3c5765c02200fd846547b
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-11-09 11:47:00 +0300

    initial commit

---
+++ cards/src/components/card.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from 'react';
+
+
+function Card1() {
+    const urlPath = 'https://lit-depths-86097.herokuapp.com/blogs'
+
+    const [data, setData] = useState([])
+
+   
+useEffect(()=>{
+    fetch(urlPath)
+    .then(res=>res.json())
+    .then(json =>{
+        setData(json)
+    })
+}, [])
+
+
+
+    return(
+        <div className='main'>
+            <div className='maindiv'>
+            
+            </div>
+            
+
+             {data.map((reviews)=>( 
+                <div key={reviews.id} className='card'>
+                    <div className='image'><img src={reviews.image} /></div>
+                <h4>{reviews.rating}</h4>
+                <h3>{revies.title}</h3>
+                <em> ~ {reviews.content}</em>
+                </div>
+
+            ))} 
+        </div>
+    )
+}
+export default Card1
