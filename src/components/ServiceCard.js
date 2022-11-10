2c62d8251971877791984e7981cecb5ff568a31a
Daniel Ngoya <46535202+BlackHawk0@users.noreply.github.com>
2022-11-10 19:44:59 +0300

    Pages (#10)
    * completed footer
    
    * fixed navbar and home image
    
    * fix: Navbar and home page
    
    * removed services
    
    * added top service page and styling
    
    * added testimonias
    
    * added testimonias
    
    * appointment form
    
    * merged services section with home
    
    * fix:testimonials
    
    Co-authored-by: rebykimani <beckireby15@gmail.com>

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import {useState, useEffect} from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import ImageList from '@material-ui/core/ImageList';
-import ImageListItem from '@material-ui/core/ImageListItem';
-import ImageListItemBar from '@material-ui/core/ImageListItemBar';
-import IconButton from '@material-ui/core/IconButton';
+import React from "react";
+import { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import ImageList from "@material-ui/core/ImageList";
+import ImageListItem from "@material-ui/core/ImageListItem";
+import ImageListItemBar from "@material-ui/core/ImageListItemBar";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@mui/material/Button";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
     backgroundColor: theme.palette.background.paper,
   },
   imageList: {
@@ -19,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
     height: 750,
   },
   icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
+    color: "rgba(255, 255, 255, 0.54)",
   },
 }));
 
@@ -27,38 +28,60 @@ export default function TitlebarImageList() {
   const [serviceTypes, setServiceType] = useState([]);
 
   const serviceTypeAPI = "https://eptome-parlor.herokuapp.com/service_types";
-  
-  useEffect(() => {
-      fetch (serviceTypeAPI)
-          .then(res => res.json())
-          .then(serviceType => setServiceType(serviceType))
-  }
-  , [])
 
+  useEffect(() => {
+    fetch(serviceTypeAPI)
+      .then((res) => res.json())
+      .then((serviceType) => setServiceType(serviceType));
+  }, []);
 
   const classes = useStyles();
 
   return (
     <div>
-    <h2>Our Services</h2>
+      <h2>Our Services</h2>
       <div className={classes.root}>
         <ImageList rowHeight={180} className={classes.imageList}>
-          <ImageListItem key="Subheader" cols={2} style={{ height: 'auto' }}>
-          </ImageListItem>
+          <ImageListItem
+            key="Subheader"
+            cols={2}
+            style={{ height: "auto" }}
+          ></ImageListItem>
           {serviceTypes.map((serviceType) => (
             <ImageListItem key={serviceType.imageURL}>
-              <img src={serviceType.imageURL} alt={serviceType.service_type_name} />
+              <img
+                src={serviceType.imageURL}
+                alt={serviceType.service_type_name}
+              />
               <ImageListItemBar
                 title={serviceType.service_type_name}
-                subtitle={<span>Description: {serviceType.service_type_description}</span>}
+                subtitle={
+                  <span>
+                    Description: {serviceType.service_type_description}
+                  </span>
+                }
                 actionIcon={
-                  <IconButton aria-label={`info about ${serviceType.service_type_name}`} className={classes.icon}>
+                  <IconButton
+                    aria-label={`info about ${serviceType.service_type_name}`}
+                    className={classes.icon}
+                  >
                     {/* <InfoIcon /> */}
                   </IconButton>
                 }
               />
             </ImageListItem>
           ))}
+          <Button
+            sx={{
+              backgroundColor: "#A52A2A",
+              padding: "15px 20px",
+              color: "#ffffff",
+              textAlign: "center",
+            }}
+            href="./services"
+          >
+            View More
+          </Button>
         </ImageList>
       </div>
     </div>
