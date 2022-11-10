0ff0258b6486dc6198bca39e45062fb783da8f8f
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-11-10 10:34:19 +0300

    This commit adds the post review page to the project

--- cards/src/components/card2.js
+++ cards/src/components/card2.js
@@ -1,19 +1,54 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Axios from "axios"
 
-function Card2() {
+
+function Booking() {
+
+    const url = 'https://eptome-parlor.herokuapp.com/reviews'
+    const [data, setData] = useState({
+        name: ' ',
+        image: ' ',
+        rating: ' ',
+        message: ' '
+      })
+
+      function handle(e) {
+        const newData = {...data}
+        newData[e.target.id] = [e.target.value]
+        setData(newData)
+        console.log(newData)
+      }
+
+
+      function addDataForm(e) {
+        e.preventDefault()
+
+        
+        Axios.post(url, {
+          name: data.name,
+          image : data.imageURL,
+          rating : data.rating,
+          message: data.message,
+        })
+        .then(res=>console.log(res))
+
+        e.reset()
+    }
+    
+
     return(
-        <div>
-            <h3>Our Services</h3>
-            <p>Spread over two floors, our beautiful spa offer a soothing environment in which you can rest, relax and feel competely rejuvenated.
-
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-             standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-           remaining essentially unchanged. 
-              It was popularised in the with theLorem Ipsum is simply dummy text of the printing and eentially unchanged.
-             </p>
-             
-
+        <div className="div">
+            <h3 className="book">we would love your feedback</h3>
+            <br />
+            <form  onSubmit={(e)=>addDataForm(e)}>
+      
+               <span><p className="label">Name</p><input onChange={(e)=>handle(e)} type='text' id="title" value={data.name} required name="title" placeholder="title" /></span> 
+               <span><p className="label">Email</p><input onChange={(e)=>handle(e)} type='file' id="title" value={data.imageURL} required name="title" placeholder="title" /></span> 
+               <span><p className="label">Tel</p><input onChange={(e)=>handle(e)} type='text' id="title" value={data.message} required name="title" placeholder="title" /></span>
+               <span><p className="label">Date</p><input onChange={(e)=>handle(e)} type='number' id="title" value={data.rating} required name="title" placeholder="title" /></span>
+               <span><input onChange={(e)=>handle(e)} type='submit'  className="submit"/></span> 
+            </form>
         </div>
     )
 }
-export default Card2
+export default Booking
