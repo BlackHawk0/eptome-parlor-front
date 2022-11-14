82ed6c1a9e7beb1ed66b530d966189d61150b9b7
StephenKairu <108527087+StephenKairu@users.noreply.github.com>
2022-11-14 10:26:43 +0300

    Services ft (#15)
    * Add navlinks
    
    * Add service-type layout
    
    * Fetch service-types
    
    * Edit service-type text
    
    * Add about page route and navlink
    
    * Add staff images
    
    * Add about text content
    
    * Fix about page orientation
    
    * Added review form and components
    
    * Style the reviews
    
    * Add upvote button
    
    * Update README
    
    * Restyle service and review card
    
    * Style reviews

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState }  from 'react'
 import './testimonial.css'
+
 const Testimonials = () => {
    const [testimonials, setTestimonials] = useState([])
    useEffect(() => {
