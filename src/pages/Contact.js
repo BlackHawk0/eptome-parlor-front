40f285dddb94906b9f8bc7ca66bb8251beee0f07
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-11-14 14:55:30 +0300

    This commit adds the styling to the contact us page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,18 +4,24 @@ import './contact.css'
 function Contact() {
     return (
         <div className="contact-container">
-            <div className="contact-section">
+            <div><img src="https://www.massagemag.com/wordpress/wp-content/uploads/1_2_Conversations.jpg" alt="image"/></div>
+            <div className='feature'>
+            {/* <div className="contact-section">
                 <h1>Contact Section</h1>
-            </div>
+            </div> */}
+            <h1>Contact Us</h1>
             <div className="contact-form">
             <form className="main-form">
-                <h1>Contact Us</h1>     <br/>
-                <input type="text" placeholder="Your Name" className="input-style"/><br/><br/>
-                <input type="email" placeholder="Your Email" className="input-style"/><br/><br/>
-                <textarea placeholder="Type Message" className="input-style"></textarea><br/><br/>
-                <button className="btn">Send Message</button>
+                     <br/>
+                <label>your name: </label><input type="text" placeholder="Your Name" className="input-style"/><br/><br/>
+                <label>email: </label><input type="email" placeholder="Your Email" className="input-style"/><br/><br/>
+                <label>message: </label><textarea placeholder="Type Message" className="input-style"></textarea><br/><br/>
+                <button className="btns">Send Message</button>
             </form>
             </div>
+
+            </div>
+
         </div>
     )
 }
