ca06c7be32a35c4b090f8d7b4f2a7a45e6ffe50e
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-11-14 15:48:08 +0300

    add booking

--- src/components/AppointmentForm.js
+++ src/components/AppointmentForm.js
@@ -44,8 +44,8 @@ export default function AppointmentForm() {
       console.log("VALUES: ", values);
       console.log("in here: ", values);
       const postData = async () => {
-        // await fetch("https://eptome-parlor-beauty.herokuapp.com/bookings/new", {
-        await fetch("http://localhost:9292/bookings/new", {
+        await fetch("https://eptome-parlor-beauty.herokuapp.com/bookings/new", {
+        // await fetch("http://localhost:9292/bookings/new", {
           method: "POST",
           body: JSON.stringify({
               customer_name: values.customer_name,
